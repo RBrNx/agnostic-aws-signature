a10c175d696aa32605b582cc2f5c8db878dcdcad
Conor Watson <conor@odro.co.uk>
2020-08-02 10:14:38 +0100

    fix: failing buildAuthorizationHeader test

--- test/buildAuthorizationHeader.test.js
+++ test/buildAuthorizationHeader.test.js
@@ -15,9 +15,9 @@ test('buildAuthorizationHeader returns an AWS Sig v4 Auth headers', () => {
   const credentialScope = buildCredentialScope(dateAsString, region, 'execute-api');
   const stringToSign = buildStringToSign(dateAsString, credentialScope, hashedCanonicalRequest);
   const signingKey = calculateSigningKey(fakeSecretKey, dateAsString, region, 'execute-api');
-  const signature = hmac(signingKey, stringToSign);
+  const signature = hmac(signingKey, stringToSign).toString();
 
   expect(buildAuthorizationHeader(fakeAccessKey, credentialScope, testRequestHeaders, signature)).toEqual(
-    'AWS4-HMAC-SHA256 Credential=ASIAWXW4R5NWN4CD3436/20200702/eu-west-2/execute-api/aws4_request, SignedHeaders=accept;content-type;host;x-amz-date, Signature=4279056b10deb760c5fdc276d0ee63a24a43bca2200ea25ab8494730cefad4a1',
+    'AWS4-HMAC-SHA256 Credential=ASIAWXW4R5NWN4CD3436/20200702/eu-west-2/execute-api/aws4_request, SignedHeaders=accept;content-type;host;x-amz-date, Signature=a7870b1327b30ba634e5c715ab19dd39d7e200a38cd6e6f8fdca7fc48a27d218',
   );
 });
