9684f9dd8c44f319f6ef8216b21f4220a07ae2c1
Conor Watson <conor@odro.co.uk>
2020-08-02 10:15:07 +0100

    feat: added official aws example to calculateSigningKey test

--- test/calculateSigningKey.test.js
+++ test/calculateSigningKey.test.js
@@ -2,7 +2,13 @@ const { calculateSigningKey } = require('../src/helpers');
 const { fakeSecretKey, dateAsString, region } = require('./variables');
 
 test('calculateSigningKey returns an AWS Sig v4 Signing Key', () => {
-  expect(calculateSigningKey(fakeSecretKey, dateAsString, region, 'execute-api')).toEqual(
-    '7acecfed2001ab2b2fe4f6c69954f2666196b2556c0c69c5486c03f99f04aab8',
+  // Official AWS Examples
+  // https://docs.aws.amazon.com/general/latest/gr/signature-v4-examples.html#signature-v4-examples-javascript
+  expect(calculateSigningKey('wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY', '20120215', 'us-east-1', 'iam').toString()).toEqual(
+    'f4780e2d9f65fa895f9c67b32ce1baf0b0d8a43505a000a1a9e090d414db404d',
+  );
+
+  expect(calculateSigningKey(fakeSecretKey, dateAsString, region, 'execute-api').toString()).toEqual(
+    '2043104976b5a59e0638e4e0cbc95a48921d6c585674f364f28d3dcd4a7266c4',
   );
 });
